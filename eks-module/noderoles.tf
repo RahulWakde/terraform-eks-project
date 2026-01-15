80668ced0c891e65230c855068577e57933c904d
antony-a-n <antonyan8904@gmail.com>
2026-01-15 23:16:32 +0530

    updated role name

--- eks-module/noderoles.tf
+++ eks-module/noderoles.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "node-roles" {
-  name = "${var.eks}-node-role"
+  name = "${var.eks}-${var.env}-node-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
